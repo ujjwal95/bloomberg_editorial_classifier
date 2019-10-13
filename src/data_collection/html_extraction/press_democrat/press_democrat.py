41f9e2cc24b978aa79298d20b65e86674889e44a
aastha99 <aastha96127@gmail.com>
2019-10-13 15:12:05 -0400

    Code update

--- src/data_collection/html_extraction/press_democrat/press_democrat.py
+++ src/data_collection/html_extraction/press_democrat/press_democrat.py
@@ -17,58 +17,57 @@ class Press_Democrat:
 		ctr = -1
 		writer = csv.writer(open('output_press_dem.csv', mode='a'), delimiter=',')
 		for i, row in (self.sub_file.iterrows()):
-			if i>2168:
-				link = row['URL']
-				ctr+=1
-				r = requests.get(link)
+			link = row['URL']
+			ctr+=1
+			r = requests.get(link)
 				
-				if r.status_code!=200:
-					print(ctr)
-					continue
+			if r.status_code!=200:
+				print(ctr)
+				continue
 
-				soup = BeautifulSoup(r.content, 'lxml')
-				title=''
-				date= ''
-				author=''
-				text=''
-				keyword = ''
+			soup = BeautifulSoup(r.content, 'lxml')
+			title=''
+			date= ''
+			author=''
+			text=''
+			keyword = ''
 
-				time.sleep(3)
-				try:
-					title = soup.find('meta',attrs={"property":"og:title"})["content"].encode('utf-8')
-				except:
-					print('Title not found at ', ctr)
+			time.sleep(3)
+			try:
+				title = soup.find('meta',attrs={"property":"og:title"})["content"].encode('utf-8')
+			except:
+				print('Title not found at ', ctr)
 
-				try: 
-					date = soup.find('meta',attrs={"property":"og:updated_time"})["content"]
-					date = date.split("T")[0].encode('utf-8')
-				except:	
-					print('Date not found at ', ctr)
+			try: 
+				date = soup.find('meta',attrs={"property":"og:updated_time"})["content"]
+				date = date.split("T")[0].encode('utf-8')
+			except:	
+				print('Date not found at ', ctr)
 
-				try:
-					author = soup.find('div', attrs = {"class":"byline-author bc-author"}).text.split("AND")
-					author = ','.join(author).encode('utf-8')
-				except:
-					print('Author not found at ', ctr)
+			try:
+				author = soup.find('div', attrs = {"class":"byline-author bc-author"}).text.split("AND")
+				author = ','.join(author).encode('utf-8')
+			except:
+				print('Author not found at ', ctr)
 
-				try:
-					keyword = soup.find('meta',attrs={"property":"bt:keywords"})["content"].encode('utf-8')
-				except:
-					print('Keyword not found at ', ctr)
+			try:
+				keyword = soup.find('meta',attrs={"property":"bt:keywords"})["content"].encode('utf-8')
+			except:
+				print('Keyword not found at ', ctr)
 
-				try:
-					text = soup.find('div', attrs={"class":"story-content"}).get_text().encode('utf-8')
-					print(len(text))
-				except:
-					print('Text not found at ', ctr)
+			try:
+				text = soup.find('div', attrs={"class":"story-content"}).get_text().encode('utf-8')
+				print(len(text))
+			except:
+				print('Text not found at ', ctr)
 				
-				print(title)#, date, author, keyword, text)
-				try:
-					writer.writerow([title, date, row['Type'], author, keyword, link, text])
-				except:
-					print(ctr)
+			print(title)#, date, author, keyword, text)
+			try:
+				writer.writerow([title, date, row['Type'], author, keyword, link, text])
+			except:
+				print(ctr)
 
-				time.sleep(5)
+			time.sleep(5)
 
 if __name__=="__main__":
 	p = Press_Democrat()
